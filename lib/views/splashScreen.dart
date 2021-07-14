2763e4301fafb0d2d7fef19a4669ed1faef9d307
Sayan Nath <sayannath235@gmail.com>
2021-07-14 16:25:08 +0530

    Splash Screen updated

--- lib/views/splashScreen.dart
+++ lib/views/splashScreen.dart
@@ -2,12 +2,13 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:propview/constants/uiContants.dart';
 
 import 'package:propview/services/authService.dart';
 import 'package:propview/utils/routing.dart';
-import 'package:propview/views/Admin/landingPage.dart'as ad;
-import 'package:propview/views/Manager/landingPage.dart'as man;
-import 'package:propview/views/Employee/landingPage.dart'as emp;
+import 'package:propview/views/Admin/landingPage.dart' as ad;
+import 'package:propview/views/Manager/landingPage.dart' as man;
+import 'package:propview/views/Employee/landingPage.dart' as emp;
 import 'package:propview/views/loginSceen.dart';
 
 class SplashScreen extends StatefulWidget {
@@ -30,8 +31,7 @@ class _SplashScreenState extends State<SplashScreen> {
   void navigate() async {
     var auth = await AuthService.getSavedAuth();
     if (auth != null) {
-      if (auth["role"] == "admin" ||
-          auth["role"]== "super_admin") {
+      if (auth["role"] == "admin" || auth["role"] == "super_admin") {
         Routing.makeRouting(context,
             routeMethod: 'pushReplacement',
             newWidget: ad.LandingScreen(selectedIndex: 0));
@@ -58,42 +58,23 @@ class _SplashScreenState extends State<SplashScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(0xff314B8C),
-      body: Column(
-        children: [
-          Expanded(
-            flex: 1,
-            child: Container(),
-          ),
-          Expanded(
-            flex: 14,
-            child: Center(
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Image.asset("assets/logo.png"),
-                  Text(
-                    "PropView",
-                    style: GoogleFonts.nunito(
-                        color: Colors.white,
-                        fontSize: 48,
-                        fontWeight: FontWeight.bold),
-                  ),
-                ],
-              ),
-            ),
-          ),
-          Expanded(
-            child: Text(
-              "Made with ❤ By Codebugged AI",
+      body: Container(
+        height: UIConstants.fitToHeight(640, context),
+        width: UIConstants.fitToWidth(360, context),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Image.asset("assets/logo.png"),
+            Text(
+              "PropView",
               style: GoogleFonts.nunito(
-                  fontSize: 16,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white),
+                  color: Colors.white,
+                  fontSize: 48,
+                  fontWeight: FontWeight.bold),
             ),
-            flex: 1,
-          )
-        ],
+          ],
+        ),
       ),
     );
   }
