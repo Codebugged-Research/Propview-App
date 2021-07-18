03d47a4034dbbd62182ce1ed25588f4767043e68
SAMBIT MAJHI <majhisambit2@gmail.com>
2021-07-18 11:22:36 +0530

    u

--- lib/config.dart
+++ lib/config.dart
@@ -1,5 +1,5 @@
 class Config {
-  static const String APP_VERISON = "1.0.5+6";
+  static const String APP_VERISON = "1.0.6+7";
   static const String UPLOAD_IMAGE_ENDPOINT =
       "http://68.183.247.233/api/upload/image";
   static const String STORAGE_ENDPOINT = "https://propview.sgp1.digitaloceanspaces.com/User/";
