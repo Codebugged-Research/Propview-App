88c8dcc3742eb526898c5cd5735753a41faffb4d
Weframe Tech <majhisambit2@gmail.com>
2022-09-29 15:57:16 +0530

    Revert "Update fullInspectionScreen.dart"
    This reverts commit 9fdbe09fb89b04649e6e3d0ca8f9d3c29d9dd8e3.

--- lib/views/Admin/Inspection/FullInspection/fullInspectionScreen.dart
+++ lib/views/Admin/Inspection/FullInspection/fullInspectionScreen.dart
@@ -149,11 +149,12 @@ class _FullInspectionScreenState extends State<FullInspectionScreen> {
       for (var i = 0; i < subRooms.length; i++) {
         setState(() {
           roomsAvailable.add(CustomRoomSubRoom(
-              isSubroom: true,
-              propertyRoomSubRoomId: subRooms[i].propertySubRoomId,
-              roomSubRoomName: getRoomName(subRooms[i].subRoomId) +
-                  " of " +
-                  getRoomName(subRooms[i].roomId)));
+            isSubroom: true,
+            propertyRoomSubRoomId: subRooms[i].propertySubRoomId,
+            roomSubRoomName: getRoomName(subRooms[i].subRoomId) +
+                " of " +
+                getRoomName(subRooms[i].roomId)
+          ));
         });
       }
     }
@@ -1308,20 +1309,13 @@ class _FullInspectionScreenState extends State<FullInspectionScreen> {
       SubRoomElement subRoom = subRooms.firstWhere((element) =>
           element.propertySubRoomId ==
           issueTableList[tableindex].roomsubroomId);
-      print(subRoom.facility);
       List<String> lstring = subRoom.facility.split(",");
-      print(lstring);
-      lstring.forEach((element) {
-        lint.add(int.parse(element));
-      });
+      lint = lstring.map(int.parse).toList();
     } else {
       RoomsToPropertyModel room = rooms.firstWhere((element) =>
           element.propertyRoomId == issueTableList[tableindex].roomsubroomId);
       List<String> lstring = room.facility.split(",");
-
-      lstring.forEach((element) {
-        lint.add(int.parse(element));
-      });
+      lint = lstring.map(int.parse).toList();
     }
     return Column(
       mainAxisSize: MainAxisSize.min,
